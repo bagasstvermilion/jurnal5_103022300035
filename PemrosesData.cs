ba4bef60bbf6cca3c9d96b60489d193fc9628a7f
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 17:06:03 +0700

    menambahkan class SimpleDataBase

--- PemrosesData.cs
+++ PemrosesData.cs
@@ -26,4 +26,32 @@ namespace jurnal5_103022300035
             return highest;
         }
     }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void addNewData(T data)
+        {
+            dynamic temp = (dynamic) data;
+            dynamic tempDate = (dynamic) DateTime.Now;
+            storedData.Add(temp);
+            inputDates.Add(tempDate);
+        }
+
+        public void printAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i+1} berisi: {storedData[i]}, yang disimpan pada waktu UTC {inputDates[i]}");
+            }
+        }
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ class Program
     static void Main()
     {
         PemrosesData pd = new PemrosesData();
-        Console.WriteLine(pd.DapatkanNilaiTerbesar(2.43, 4.65, 3.52));
+        Console.WriteLine("Nilai terbesar adalah: " + pd.DapatkanNilaiTerbesar(10.30, 22.30, 00.35));
+
+        Console.WriteLine();
+
+        SimpleDataBase<double> sdb = new SimpleDataBase<double>();
+
+        sdb.addNewData(10.30);
+        sdb.addNewData(22.30);
+        sdb.addNewData(00.35);
+
+        sdb.printAllData();
     }
 }
